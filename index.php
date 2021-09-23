97cb88a8da8dfba2148e29d791412cca076e111e
Angelica <abernard1@ggc.edu>
2021-09-23 18:35:21 -0400

    Added css style page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
+
 $active_group = 'default';
 $query_builder = TRUE;
 
