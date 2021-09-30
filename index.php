34c8d9db73922e865a51f49ea1d7e4434525e0e0
Angelica <abernard1@ggc.edu>
2021-09-30 17:49:02 -0400

    Changes made to the hompage

--- index.php
+++ index.php
@@ -19,18 +19,36 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     <meta charset="UTF-8">
     <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
     <title>ChemLife101 Homepage</title>
 </head>
 <body>
-    <header>
+    <div class = "grid-container">
+        <div class = "item1">
         <ul>
-            <li id="chemlife101" class="active">ChemLife101</li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Study</a></li>
+            <li id="chemlife101" class="chemlife101">ChemLife101</li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="forum.php">Forum></a><li>
+            <li><a href="study.php">Study</a></li>
             <li id="login"><a href="login.php">Login</a></li>
             <li id="signup"><a href="signUp.php">Sign Up</a></li>
         </ul>
-    </header>
+        </div>
+        <div class = "item2">
+            <ul>
+                <li id ="resources" class="resources">Resources><li>
+                <li><a href="periodic.php">Periodic Table></a><li>
+                <li><a href="conversions.php">Conversions></a><li>
+            </ul>
+        </div>
+        <div class = "item3">
+            Main
+        </div>
+        <div class = "item4">
+            <li><a href="">About Us></a><li>
+            <li><a href="">Contact Us></a><li>
+            <li><a href="references.php">ChemLife101's References></a><li>
+        </div>
+</div>
 </body>
 </html>
