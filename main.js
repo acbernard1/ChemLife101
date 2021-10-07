80ce77b99f3c0bd91bebcc35d6bc0823d135bc04
Carina <cgeorge4@ggc.edu>
2021-10-06 23:27:13 -0400

    Adding check login function

--- main.js
+++ main.js
@@ -16,6 +16,25 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
+function check_login($con) {
+    if(isset($_SESSION['user_id']))
+    {
+        $id = $_SESSION['user_id'];
+        $query = "select * from users where user_id = '$id' limit 1";
+
+        $result = mysqli_query($con, $query);
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    }
+
+    //rediredt to login
+    header("Location: login.php");
+    die;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
