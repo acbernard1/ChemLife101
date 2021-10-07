304d98c51fca3d6f3912de2a2b9b4a3834cc3c7a
Carina <cgeorge4@ggc.edu>
2021-10-06 23:49:06 -0400

    Editing signup page

--- signUp.php
+++ signUp.php
@@ -16,7 +16,7 @@ session_start();
             $user_id = random_num(50);
             $query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
 
-            mysqli_query($query);
+            mysqli_query($con, $query);
 
             header("Locarion: login.php");
             die;
