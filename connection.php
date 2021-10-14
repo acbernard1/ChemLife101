5d3164b3e0428cef993a9679c693851a15ed80d3
Angelica <abernard1@ggc.edu>
2021-10-14 17:17:20 -0400

    DB Connection Fix

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
     $dbpass = "22f1f85e";
     $dbname = "heroku_1c20d2071a3d5d8";
 
-    if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+    if($con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
     {
         die("failed to connect!");
     }
