1d55268313fafd181990f59f3740307d74838697
Angelica <abernard1@ggc.edu>
2021-10-14 17:01:17 -0400

    Fixing Connection to DB

--- connection.php
+++ connection.php
@@ -1,19 +1,12 @@
-<<?php
+<?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+    $dbhost = "us-cdbr-east-04.cleardb.com";
+    $dbuser = "b21349ad86976d";
+    $dbpass = "22f1f85e";
+    $dbname = "heroku_1c20d2071a3d5d8";
 
-$active_group = 'default';
-$query_builder = TRUE;
-
-// If Not Connect to DB
-if(!$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);)
-{
-    die("Failed to connect!");
-}
-
+    if(!$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+    {
+        die("failed to connect!");
+    }
 ?>
