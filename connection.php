4196c733e22876d4afbffe51c67b4702a520160f
Angelica <abernard1@ggc.edu>
2021-10-14 18:48:47 -0400

    DB

--- connection.php
+++ connection.php
@@ -1,19 +1,13 @@
 <?php
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+    $dbhost = "us-cdbr-east-04.cleardb.com";
+    $dbuser = "b21349ad86976d";
+    $dbpass = "22f1f85e";
+    $dbname = "heroku_1c20d2071a3d5d8";
+    $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-if(!$con)
-{
-    die("failed to connect!");
-}
+    if(!$con)
+    {
+        die("failed to connect!");
+    }
 ?>
