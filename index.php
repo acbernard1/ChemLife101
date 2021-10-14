ac299f4a7f8b1c93b8ec21cecf25182876d4d43e
Angelica <abernard1@ggc.edu>
2021-10-14 16:51:40 -0400

    Fixing Connection to DB

--- index.php
+++ index.php
@@ -4,23 +4,7 @@ session_start();
     include("connection.php");
     include("main.js");
 
-    $user_data = check_login($con);
-
-?>
-
-<?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $user_data = check_login($conn);
 
 ?>
 
@@ -33,21 +17,29 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     <title>ChemLife101 Homepage</title>
 </head>
 
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <title>ChemLife101 Homepage</title>
+</head>
+
 <body>
     <div class = "grid">
-
-        <header>
-            <a class="chemlife">ChemLife101</a>
-            <nav>
-                <ul class="navlinks">
-                    <li><a class="home" href="index.php">Home</a></li>
-                    <li><a href="forum.php">Forum</a></li>
-                    <li><a href="study.php">Study</a></li>
-                </ul>
-            </nav>
-            <a class="signup" href="signUp.php"><Button>Sign Up</Button></a>
-            <a class="login"href="login.php"><Button>Login<Button></a>
-        </header>
+      <div class= "header">
+    </div>
+       <a class="chemlife">ChemLife101</a>
+       <div class="navlinks"> <!––changed to div for easier adjustment––>
+              <a class="home link" href="index.php">Home</a>
+              <a class= "link" href="forum.php">Forum</a>
+              <a class = "link" href="study.php">Study</a>
+        </div>
+        <div class= "b-group">
+              <a class="signup" href="signUp.php"><Button>Sign Up</Button></a>
+              <a class="login"href="login.php"><Button>Login</Button></a>
+        </div>
 
         <main>
             Main
