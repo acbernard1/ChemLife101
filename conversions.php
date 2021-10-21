5790b64489eee95d979b4d9f8a5e189b73b6d562
Carina <cgeorge4@ggc.edu>
2021-10-21 14:31:44 -0400

    Adding header and footer

---
+++ conversions.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <title>ChemLife101 Homepage</title>
+</head>
+
+<body>
+    <div class = "grid">
+      <div class= "header">
+    </div>
+       <a class="chemlife">ChemLife101</a>
+       <div class="navlinks"> <!––changed to div for easier adjustment––>
+              <a class="home link" href="index.php">Home</a>
+              <a class= "link" href="forum.php">Forum</a>
+              <a class = "link" href="study.php">Study</a>
+        </div>
+        <div class= "b-group">
+              <a class="signup" href="signUp.php"><Button>Sign Up</Button></a>
+              <a class="login"href="login.php"><Button>Login</Button></a>
+        </div>
+        <footer>
+            <ul>
+                <li><a href="aboutUs.php">About Us</a><li>
+                <li><a href="contactUs.php">Contact Us</a><li>
+                <li><a href="ref.php">ChemLife101's References</a><li>
+            </ul>
+            <a href="logout.php">Log out</a>
+        </footer>
+
+    </body>
+</html>
