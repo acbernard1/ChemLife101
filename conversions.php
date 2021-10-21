0b5c3eee0353e87c379492970f27a2aa47a8f316
Carina <cgeorge4@ggc.edu>
2021-10-21 14:35:52 -0400

    Changing stylesheet

--- conversions.php
+++ conversions.php
@@ -3,8 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="index.css">
-    <title>ChemLife101 Homepage</title>
+    <link rel="stylesheet" href="main.css">
+    <title>ChemLife101: Conversions</title>
 </head>
 
 <body>
@@ -21,14 +21,4 @@
               <a class="signup" href="signUp.php"><Button>Sign Up</Button></a>
               <a class="login"href="login.php"><Button>Login</Button></a>
         </div>
-        <footer>
-            <ul>
-                <li><a href="aboutUs.php">About Us</a><li>
-                <li><a href="contactUs.php">Contact Us</a><li>
-                <li><a href="ref.php">ChemLife101's References</a><li>
-            </ul>
-            <a href="logout.php">Log out</a>
-        </footer>
-
-    </body>
-</html>
+        
