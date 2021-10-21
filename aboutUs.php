6c569f5315a57cdda892b7eeda5fea5c36e1db60
Carina <cgeorge4@ggc.edu>
2021-10-21 17:39:23 -0400

    Adding About Us page

---
+++ aboutUs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>ChemLife101: About Us</title>
+    <link rel="shortcut icon" href="/assets/favicon.ico">
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    
+</body>
+</html>
