960016b422fdb8eb140cffe8d4bcdd99980c3213
Carina <cgeorge4@ggc.edu>
2021-10-29 09:09:33 -0400

    Working on nav bar

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//Get Heroku ClearDB connection information
+/*//Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
@@ -8,7 +8,7 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);*/
 ?>
 
 <html>
