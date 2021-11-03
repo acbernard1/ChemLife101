32ca7b74aa900e94315b9e01f4c9f4946948f1fb
Carina <cgeorge4@ggc.edu>
2021-11-03 13:30:26 -0400

    editing about page

--- aboutUs.php
+++ aboutUs.php
@@ -1,5 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>ChemLife101 About Us</title>
+</head>
+<body>
+<header>
+  <h1 class="logo">ChemLife101</h1>
+  <input type="checkbox" id="nav-toggle" class="nav-toggle">
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="aboutUs.php">About</a></li>
+      <li><a href="contactUs.php">Contact Us</a></li>
+    </ul>
+  </nav>
+  <label for="nav-toggle" class="nav-toggle-label">
+    <span></span>
+  </label>
+</header>
+
+
+
+
+<div class="content">
+  <h2>Your content would go here</h2>
+</div>
+</body>
+</html>
+
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
@@ -7,23 +41,7 @@
     <link rel="shortcut icon" href="/assets/favicon.ico">
     <link rel="stylesheet" href="main.css">
 </head>
-
 <body>
-<div class = "grid">
-      <div class= "header">
-    </div>
-       <a class="chemlife">ChemLife101</a>
-       <div class="navlinks"> <!––changed to div for easier adjustment––>
-              <a class="home link" href="index.php">Home</a>
-              <a class= "link" href="forum.php">Forum</a>
-              <a class = "link" href="study.php">Study</a>
-        </div>
-    
-</body>
 
-<footer>
-            <ul>
-                <li><a href="contactUs.php">Contact Us</a><li>
-            </ul>
-        </footer>
+</body>
 </html>
